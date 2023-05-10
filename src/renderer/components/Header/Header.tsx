34c7054fa548acbca923d9eb7e2fe3ef8bf198d5
Anmol Srivastava <anmol150srivastava@gmail.com>
2023-05-10 13:34:10 +0530

    Revert "conflict fixes"
    This reverts commit 26af897a04bee6bab908b2512f1613e28a9d6b9e.

--- src/renderer/components/Header/Header.tsx
+++ src/renderer/components/Header/Header.tsx
@@ -1,68 +1,46 @@
-import { CloseIcon, SearchIcon } from '@chakra-ui/icons';
-import { Box, createMultiStyleConfigHelpers, HStack, IconButton, Input, InputGroup, InputLeftElement, InputRightElement, SimpleGrid, Tab, TabList, TabPanel, TabPanels, Tabs } from '@chakra-ui/react';
-import { dispatch } from 'd3';
-import { ChangeEventHandler, memo } from 'react';
-import {useDispatch} from 'react-redux';
-import { switchToDesigner, switchToEngine } from '../../redux/slices/settingsSlice';
+import {Box, HStack, SimpleGrid, Tab, TabList, TabPanel, TabPanels, Tabs} from '@chakra-ui/react';
+import { memo } from 'react';
 // import { DependencyManagerButton } from '../DependencyManagerButton';
-// import { SettingsButton } from '../SettingsModal';
+import { SettingsButton } from '../SettingsModal';
 // import { SystemStats } from '../SystemStats';
+import { useDispatch } from 'react-redux';
+import { switchToDesigner, switchToEngine } from '../../redux/slices/settingsSlice';
 import { AppInfo } from './AppInfo';
 import { ExecutionButtons } from './ExecutionButtons';
 import { AppDispatch, RootState } from "../../redux/store";
 
-	
-	export const Header = memo(() => {
-		const dispatch = useDispatch<AppDispatch>();
+export const Header = memo(() => {
+    const dispatch = useDispatch<AppDispatch>();
+    return (
+        <Box
+            bg="var(--header-bg)"
+            w="100%"
+        >
+            <SimpleGrid
+                columns={5}
+                h="56px"
+                w="100%"
+                spacing={3}
+            >
+                <AppInfo />
+                <HStack></HStack>
+                <ExecutionButtons />
+                <SettingsButton />
 
-		return (
-			<Tabs 
-			zIndex={99999}
-			variant={'unstyled'} 
-			bg="gray.500"
-			width={"100%"}
-			>
-			<TabList>
-			<Tab 
-			 onClick={() => dispatch(switchToEngine())}
-			fontSize={13} 
-			_selected={{bg: "var(--selected-tab-color)"}}>
-				<HStack>
-				<Box minW={"130px"}>
-					MachineFlow
-				</Box>
-				</HStack>
-			</Tab>
-			<Tab 
-			onClick={() => dispatch(switchToDesigner())}
-			fontSize={13} 
-			_selected={{bg: "var(--selected-tab-color)"}}
-			borderRight={"1px solid var(--selected-tab-color)"}
-			>
-				<HStack>
-				<Box minW={"130px"}>
-					UI Builder
-				</Box>
-				</HStack>
-				{/* <IconButton
-                    aria-label='close tab'
-					_hover={{
-						bg: "white", 
-						color:"var(--gray-500)"
-					}}
-                    icon={<CloseIcon scale={0.9}/>}
-                    size="xs"
-                    variant="filled"
-                /> */}
-			</Tab>
-			</TabList>
-			</Tabs>
-			);
-		});
-		
-		// <Center w="full">
-		//     <SystemStats />
-		//     <DependencyManagerButton />
-		//     <SettingsButton />
-		// </Center>
-		
+                <Tabs align='end'>
+                    <TabList>
+                        <Tab onClick={() => dispatch(switchToEngine())}>Engine</Tab>
+                        <Tab onClick={() => dispatch(switchToDesigner())}>Designer</Tab>
+                    </TabList>
+                </Tabs>
+            </SimpleGrid>
+        </Box>
+    );
+});
+
+// <Center w="full">
+//     <SystemStats />
+//     <DependencyManagerButton />
+//     <SettingsButton />
+// </Center>
+
